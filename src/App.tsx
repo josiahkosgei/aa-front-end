3423ebf798d726abf027118b208d4f9ce1bf364d
Josiah Kosgei <josiahkosgei36@gmail.com>
2021-06-19 20:08:05 +0300

    Design app main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { BellIcon, MenuIcon, XIcon, PaperClipIcon } from '@heroicons/react/outline'
 
 const navigation = ['Dashboard', 'Team', 'Projects', 'Calendar', 'Reports']
+const hospitalVisits = [{ visits: 7, hospital: 'Kiambiu' }, { visits: 12, hospital: 'Mukuru Kwa Ruben' }, { visits: 26, hospital: 'Mukuru Kwa Njenga' }, { visits: 38, hospital: 'Baba Dogo' }, { visits: 41, hospital: 'Kosovo' }, { visits: 24, hospital: 'Mukuru Kayaba' }]
 const profile = ['Your Profile', 'Settings', 'Sign out']
 
 function classNames(...classes: string[]) {
@@ -27,7 +28,7 @@ function App() {
                   </svg>
                 </div>
               </li>
-              <li className="flex w-full justify-between text-gray-400 hover:text-purple-600 cursor-pointer items-center mb-6">
+              <li className="flex w-full justify-between text-purple-600 hover:text-purple-600 cursor-pointer items-center mb-6">
                 <div className="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width={48} height={64} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
@@ -90,7 +91,7 @@ function App() {
                             <button className="focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                               <div className="ml3">
                                 <div className="text-base font-medium leading-none text-gray-800">Tom Cook</div>
-                                <div className="text-sm font-medium leading-none text-gray-600">tom@example.com</div>
+                                <div className="text-sm font-medium leading-none text-gray-600">tom@accessafya.com</div>
                               </div>
                             </button>
 
@@ -159,7 +160,7 @@ function App() {
                           </div>
                           <div className="ml-3">
                             <div className="text-base font-medium leading-none text-white">Tom Cook</div>
-                            <div className="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+                            <div className="text-sm font-medium leading-none text-gray-400">tom@accessafya.com</div>
                           </div>
                           <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                             <span className="sr-only">View notifications</span>
@@ -187,25 +188,169 @@ function App() {
           {/* Page title starts */}
           <div className="container mx-auto py-6 md:w-9/10 w-11/12 px-6">
             <div>
-              <h4 className="text-2xl font-bold leading-tight text-gray-800">Analytics</h4>
+              <h5 className="text-2xl font-bold leading-tight text-gray-800">Analytics</h5>
             </div>
           </div>
           {/* Page title ends */}
           <div className="container mx-auto py-6 md:w-9/10 w-11/12 px-6">
-            <div className="flex gap-8 flex-nowrap justify-around h-screen">
+            <div className="flex gap-8 flex-nowrap justify-around h-5/6">
               <div className="w-1/3 shadow bg-white">
-                <div className="flex items-center justify-between w-full h-5/6">
-                  <div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded bg-white shadow">
-                    <div className="w-full h-5/6 dark:bg-gray-800">
+                <div className="flex itemscenter justify-between w-full h-screen">
+                  <div className="flex flex-col lg:flex-row w-full items-start lg:items-start rounded bg-white shadow">
+                    <div className="w-full h-full dark:bg-gray-800 justify-start px-4 py-2">
+                      <div className="flex w-1/4 justify-center">
+                        <h6 className="text-sm font-bold leading-tight text-gray-300">VISITS</h6>
+                      </div>
+                      <ul className="flex flex-col items-center justify-items-stretch justify-center">
+
+                        {hospitalVisits.map((item, itemIdx) =>
+                          itemIdx === 0 ? (
+                            <Fragment key={item.hospital}>
+                              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                              <li className="border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white hover:bg-purple-600 cursor-pointer mt-6">
+                                <div className="w-0 flex-1 flex items-center">
+                                  <span className="ml-2 flex-1 w-0 truncate"><strong><span className="hover:text-white text-purple-600 ">{item.visits}</span>  {item.hospital}</strong></span>
+                                </div>
+                                <div className="ml-4 flex-shrink-0">
+                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                  </svg>
+                                </div>
+                              </li>
+                            </Fragment>
+                          ) : (
+
+                            <li key={item.hospital} className="border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white hover:bg-purple-600 cursor-pointer mt-6">
+                              <div className="w-0 flex-1 flex items-center">
+                                <span className="ml-2 flex-1 w-0 truncate"><strong><span className="hover:text-white text-purple-600">{item.visits}</span>  {item.hospital}</strong></span>
+                              </div>
+                              <div className="ml-4 flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                </svg>
+                              </div>
+                            </li>
+                          )
+                        )}
+                      </ul>
                     </div>
                     {/* <div className="w-full lg:w-1/3 h-24 dark:border-gray-700 lg:h-5/6 border-t lg:border-t-0 lg:border-r lg:border-l lg:rounded-r dark:bg-gray-700 bg-gray-100" /> */}
                   </div>
                 </div>
               </div>
-              <div className="w-2/3 shadow bg-white">
+              <div className="w-2/3 bg-gray-200">
                 <div className="flex items-center justify-between w-full">
-                  <div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded bg-white shadow">
-                    <div className="w-full h-5/6 dark:bg-gray-800" />
+                  <div className="flex flex-col w-full">
+                    <div className="w-full shadow rounded h-3/6 bg-white px-4 py-2">
+                      <h4 className="text-sm font-bold leading-tight text-gray-300">KEY ISSUES</h4>
+                      <ul className="grid grid-cols-4 gap-1 w-full mb-4">
+
+                        {hospitalVisits.map((item, itemIdx) =>
+                          itemIdx === 0 ? (
+                            <Fragment key={item.hospital}>
+                              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                              <li className="border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white hover:bg-purple-600 cursor-pointer mt-6">
+                                <div className="w-0 flex-1 flex items-center">
+                                  <span className="ml-2 flex-1 w-0 truncate"><strong><span className="hover:text-white text-purple-600 ">{item.visits}</span>  {item.hospital}</strong></span>
+                                </div>
+                                <div className="ml-4 flex-shrink-0">
+                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                  </svg>
+                                </div>
+                              </li>
+                            </Fragment>
+                          ) : (
+
+                            <li key={item.hospital} className="border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white hover:bg-purple-600 cursor-pointer mt-6">
+                              <div className="w-0 flex-1 flex items-center">
+                                <span className="ml-2 flex-1 w-0 truncate"><strong><span className="hover:text-white text-purple-600">{item.visits}</span>  {item.hospital}</strong></span>
+                              </div>
+                              <div className="ml-4 flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                </svg>
+                              </div>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </div>
+                    <div className="w-full shadow rounded h-3/6 bg-white px-4 py-2 mt-6">
+                      <h4 className="text-sm font-bold leading-tight text-gray-300">MONITORING PERIOD</h4>
+                      <div className="flex gap-5 mb-4">
+                        <div className="w-4/5 flex gap-6">
+                          <div className="w-3/5">
+                            <div className="border border-gray-200 rounded-md inline-flex w-full" role="group" aria-label="Monitoring Period">
+                              <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 rounded-l-lg focus:shadow-outline hover:bg-indigo-800">Day</button>
+                              <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 focus:shadow-outline hover:bg-indigo-800">Week</button>
+                              <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 focus:shadow-outline hover:bg-indigo-800">Month</button>
+                              <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 rounded-r-lg focus:shadow-outline hover:bg-indigo-800">Year</button>
+                            </div>
+                          </div>
+                          <div className="w-2/5">
+                            <div className="border border-gray-200 rounded-md inline-flex " role="group" aria-label="Button group">
+                              <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 rounded-l-lg focus:shadow-outline hover:bg-indigo-800">Left</button>
+                              <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 rounded-r-lg focus:shadow-outline hover:bg-indigo-800">Month</button>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="w-1/5">
+
+                        </div>
+                      </div>
+                    </div>
+                    <div className="h-2/6 mt-6">
+                      <div className="grid grid-cols-3 gap-5 w-full mb-4">
+                        <div className="bg-white border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white cursor-pointer mt-6">
+                          <h4 className="text-sm font-bold leading-tight text-gray-700">Foot fall</h4>
+                           </div>
+                        <div className="bg-white border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white cursor-pointer mt-6">
+                          <h4 className="text-sm font-bold leading-tight text-gray-700">Patient Satisfaction</h4>
+                        </div>
+                        <div className="bg-white border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white cursor-pointer mt-6">
+                          <h4 className="text-sm font-bold leading-tight text-gray-700">Revenue</h4>
+                        </div>
+
+                      </div>
+                    </div>
+
+                    <div className="w-full shadow rounded h-3/6 bg-white px-4 py-2 mt-6">
+                      <h4 className="text-sm font-bold leading-tight text-gray-300">KEY ISSUES</h4>
+                      <ul className="grid grid-cols-4 gap-1 w-full mb-4">
+
+                        {hospitalVisits.map((item, itemIdx) =>
+                          itemIdx === 0 ? (
+                            <Fragment key={item.hospital}>
+                              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                              <li className="border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white hover:bg-purple-600 cursor-pointer mt-6">
+                                <div className="w-0 flex-1 flex items-center">
+                                  <span className="ml-2 flex-1 w-0 truncate"><strong><span className="hover:text-white text-purple-600 ">{item.visits}</span>  {item.hospital}</strong></span>
+                                </div>
+                                <div className="ml-4 flex-shrink-0">
+                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                  </svg>
+                                </div>
+                              </li>
+                            </Fragment>
+                          ) : (
+
+                            <li key={item.hospital} className="border border-gray-200 rounded-md pl-3 pr-4 py-3 flex items-center text-sm flex w-10/12 text-gray-400 hover:text-white hover:bg-purple-600 cursor-pointer mt-6">
+                              <div className="w-0 flex-1 flex items-center">
+                                <span className="ml-2 flex-1 w-0 truncate"><strong><span className="hover:text-white text-purple-600">{item.visits}</span>  {item.hospital}</strong></span>
+                              </div>
+                              <div className="ml-4 flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                </svg>
+                              </div>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </div>
                     {/* <div className="w-full lg:w-1/3 h-24 dark:border-gray-700 lg:h-5/6 border-t lg:border-t-0 lg:border-r lg:border-l lg:rounded-r dark:bg-gray-700 bg-gray-100" /> */}
                   </div>
                 </div>
